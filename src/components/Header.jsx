4d1f6d1472b092137990a9ba6ee91f9f87225611
Morning <morning@admins-iMac-3.local>
2023-02-16 12:37:39 +0800

    meteo

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <>
+      <div className="header">
+        <select>
+          <option value="mn">Ulaanbaatar</option>
+          <option value="ru">Moscow</option>
+          <option value="ch">Beijing</option>
+        </select>
+      </div>
+    </>
+  );
+}
