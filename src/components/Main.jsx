c293f605890efd667be4bc231bbac6bfb9d7e0cf
Morning <morning@admins-iMac-3.local>
2023-03-27 13:28:13 +0800

    lac

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,37 @@
+import axios from "axios";
+
 export default function Main() {
+  const location = {
+    location: {
+      type: "Point",
+      coordinates: [106.934593, 47.923756],
+    },
+  };
+
+  function clickHandlar() {
+    // if (navigator.geolocation) {
+    //   navigator.geolocation.getCurrentPosition(showPosition);
+    // } else {
+    //   console.log("Geolocation is not supported by this browser.");
+    // }
+
+    // function showPosition(position) {
+    //   const latitude = position.coords.latitude;
+    //   const longitude = position.coords.longitude;
+    //   console.log("Latitude: " + latitude);
+    //   console.log("Longitude: " + longitude);
+
+    // }
+    axios.post("http://localhost:8080/nearrestaurant", location).then((res) => {
+      console.log(res);
+    });
+  }
+
   return (
     <>
-      <div>Main</div>
+      <div>
+        <button onClick={clickHandlar}>click</button>
+      </div>
     </>
   );
 }
